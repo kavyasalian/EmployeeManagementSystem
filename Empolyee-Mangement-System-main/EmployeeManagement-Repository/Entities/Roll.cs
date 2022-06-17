e4cb8e1772d5069aa9e704cb139c1d118bd65fa2
9878navya <102017871+9878navya@users.noreply.github.com>
2022-06-17 19:48:03 +0530

    roll entities added

---
+++ Empolyee-Mangement-System-main/EmployeeManagement-Repository/Entities/Roll.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EmployeeManagement_Repository.Entities
+{
+    public partial class Roll
+    {
+        public int RollId { get; set; }
+        public string RollName { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
