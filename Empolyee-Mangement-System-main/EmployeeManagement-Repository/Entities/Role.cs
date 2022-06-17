435af3415b252c3c77fc6298fd047e885b512b14
9878navya <102017871+9878navya@users.noreply.github.com>
2022-06-17 20:42:25 +0530

    changed entities

--- Empolyee-Mangement-System-main/EmployeeManagement-Repository/Entities/Role.cs
+++ Empolyee-Mangement-System-main/EmployeeManagement-Repository/Entities/Role.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 namespace EmployeeManagement_Repository.Entities
 { 
     public partial class Role
-    {
+    {   
         public int RoleId { get; set; }
         public string RoleName { get; set; }
         public DateTime DateCreated { get; set; }
