c6f89e4e2118909c58f8481cd6586edb9dd9d340
Kavya Salian <kavya.salian@aykansoft.com>
2022-06-20 15:46:45 +0530

    Adding spa services

--- Empolyee-Mangement-System-main/EmployeeManagement-Web/Program.cs
+++ Empolyee-Mangement-System-main/EmployeeManagement-Web/Program.cs
@@ -4,25 +4,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddControllersWithViews()
-    .AddNewtonsoftJson(options =>
-    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-);
-var app = builder.Build();
+builder.Services.AddEndpointsApiExplorer();builder.Services.AddSwaggerGen();builder.Services.AddControllersWithViews()    .AddNewtonsoftJson(options =>    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);builder.Services.AddCors(options =>{    options.AddPolicy(name: "CorsPolicy",                      builder =>                      {                          builder.WithOrigins("http://localhost:4200/").                                              AllowAnyMethod().                                              AllowAnyHeader().                                              AllowCredentials();                      });});builder.Services.AddSpaStaticFiles(configuration =>{    configuration.RootPath = "ClientApp/dist/NGO";});var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+if (app.Environment.IsDevelopment()){
 
-app.UseHttpsRedirection();
+    app.UseCors(x => x.AllowAnyMethod().                       AllowAnyHeader().                       WithHeaders().                       AllowCredentials().                       SetIsOriginAllowed(origin =>                       true));    app.UseSwaggerUI();    app.UseSwagger();}app.UseHttpsRedirection();app.UseStaticFiles();app.UseRouting();app.UseEndpoints(endpoints =>{    endpoints.MapControllerRoute(        name: "default",        pattern: "{controller}/{action=Index}/{id?}");    endpoints.MapControllers();});
 
-app.UseAuthorization();
+//app.MapFallbackToFile("index.html"); ;
+app.UseSpa(spa =>{    spa.Options.SourcePath = "ClientApp";    if (app.Environment.IsDevelopment())    {        spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
 
-app.MapControllers();
-
-app.Run();
+        // NOTE: Disable above line and enable below line to trigger angular from dev server.
+        //spa.UseAngularCliServer(npmScript: "start");
+    }});app.UseHttpsRedirection();app.UseAuthorization();app.MapControllers();app.Run();
