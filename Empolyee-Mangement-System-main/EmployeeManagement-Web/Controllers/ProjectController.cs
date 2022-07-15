1a2521e1346fe15e0969170302f0e38d43cd5beb
Likith G <Hp@DESKTOP-R8VNQH6>
2022-07-15 20:06:18 +0530

    Project API Get all projects

---
+++ Empolyee-Mangement-System-main/EmployeeManagement-Web/Controllers/ProjectController.cs
@@ -0,0 +1,57 @@
+﻿using EmployeeManagement_Business;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EmployeeManagement_Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly ILogger<ProjectController> _logger;
+        private readonly ProjectBuisness projectBusiness;
+        public ProjectController(ILogger<ProjectController> logger)
+        {
+            _logger = logger;
+            projectBusiness = new ProjectBuisness();
+        }
+
+        // GET: api/<ProjectController>
+        [HttpGet("GetAllProjects")]
+        public async Task<IActionResult> GetAllProject()
+        {
+            var projects = await projectBusiness.GetAllProjectAsync();
+            if (projects != null)
+            {
+                return Ok(projects);
+            }
+            return NoContent();
+        }
+
+        // GET api/<ProjectController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ProjectController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProjectController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProjectController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Empolyee-Mangement-System-main/EmployeeManagement.Data/ProjectGetModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeManagement.Data
+{
+    public class ProjectGetModel
+    {
+        public int ProjectId { get; set; }
+        public String ProjectName { get; set; } = null!;
+        public String ProjectDesc { get; set; } = null!;
+        public DateTime StartDate { get; set; } 
+        public DateTime EndDate { get; set; } 
+    }
+}
