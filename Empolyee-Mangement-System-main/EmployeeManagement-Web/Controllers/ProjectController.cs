8d536529c26cb8d35391cebd94527375f3715f19
Likith G <Hp@DESKTOP-R8VNQH6>
2022-07-15 20:14:06 +0530

    Get all projects

--- Empolyee-Mangement-System-main/EmployeeManagement-Web/Controllers/ProjectController.cs
+++ Empolyee-Mangement-System-main/EmployeeManagement-Web/Controllers/ProjectController.cs
@@ -28,30 +28,5 @@ namespace EmployeeManagement_Web.Controllers
             }
             return NoContent();
         }
-
-        // GET api/<ProjectController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<ProjectController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<ProjectController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<ProjectController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
