94be8239d962cd2b8eb5dffa9d4bb82f38754d21
IamLikithG <likithganesh2001@gmail.com>
2022-10-21 20:34:08 +0530

    commit

--- Empolyee-Mangement-System-main/EmployeeManagement-Web/ClientApp/src/environments/environment.ts
+++ Empolyee-Mangement-System-main/EmployeeManagement-Web/ClientApp/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiUrl: "https://localhost:44349/api",
+  apiUrl: "https://localhost:54973/api",
   production: false
 };
 
